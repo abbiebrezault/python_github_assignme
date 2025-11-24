9dac0e9b60ce1b8f26b480fb957a45a05fd71e6f
Abbie Brezault <abbiebrezault@usf.edu>
2025-11-24 00:04:42 -0500

    Add formatted output displaying calculation results

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 print("Welcome to my Python program!")
 hours = input("How many hours did you study today? ")
 #mejmesjkimg
+print(f"You are on track to study {weekly_hours} hours this week.")
+#gjudgdeujgdg
